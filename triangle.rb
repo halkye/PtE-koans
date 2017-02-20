6e9c21268517f380f61708de12b273b7ddb26453
Piotr Halkiewicz <halkye@gmail.com>
2017-02-20 20:15:36 +0100

    Refactoring

--- triangle.rb
+++ triangle.rb
@@ -14,40 +14,37 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
-  return :equilateral if all_sides_are_equal(a, b, c)
-  return :scalene     if each_side_is_different(a, b, c)
-  return :isosceles   if two_sides_are_equal(a, b, c)
+  raise_error("Sides must be higher than zero")                 if sides_are_negative?(a, b, c)
+  raise_error("Sum of two sides must be higher than third one") if triangle_not_closing?(a, b, c)
 
-  if sides_are_negative(a, b, c) 
-    raise TriangleError, "Sides must be higher than zero"
-  end
-
-  if triangle_not_closing(a, b, c)
-    raise TriangleError, "Sum of two sides must be higher than third one" 
-  end
+  return :equilateral if all_sides_are_equal?(a, b, c)
+  return :scalene     if each_side_is_different?(a, b, c)
+  return :isosceles   if two_sides_are_equal?(a, b, c)
 end
 
-#Possible failures
-def sides_are_negative(a, b, c)
+def sides_are_negative?(a, b, c)
   a <= 0 || b <= 0 || c <= 0
 end
 
-def triangle_not_closing(a, b, c)
+def triangle_not_closing?(a, b, c)
   ((a + b <= c) || (a + c <= b) || (b + c <= a))
 end
-#Defining side lengths
-def all_sides_are_equal(a, b, c) 
+
+def all_sides_are_equal?(a, b, c)
   a == b && b == c
 end
 
-def each_side_is_different(a, b, c) 
+def each_side_is_different?(a, b, c)
   a != b && b != c && a != c
 end
 
-def two_sides_are_equal(a, b, c)
+def two_sides_are_equal?(a, b, c)
   (a==b && a!=c) || (a==c && a!=b) || (b==c && b!=a)
 end
 
+def raise_error(message)
+  raise TriangleError, message
+end
 
 # Error class used in part 2.  No need to change this code.
 class TriangleError < StandardError
