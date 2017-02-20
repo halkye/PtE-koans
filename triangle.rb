a3fa71d852e50682088cbfc65341a03cecb15374
Piotr Halkiewicz <halkye@gmail.com>
2017-02-20 18:27:08 +0100

    all methods chcecking type of triangle are working but TriangleError fails

--- triangle.rb
+++ triangle.rb
@@ -14,23 +14,40 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
-  if a <= 0 || b <= 0 || c <= 0
-    raise TriangleError, "Sides must be higher than zero"
+  return :equilateral if all_sides_are_equal(a, b, c)
+  return :scalene     if each_side_is_different(a, b, c)
+  return :isosceles   if two_sides_are_equal(a, b, c)
 
-  elsif ( (a + b <= c) || (a + c <= b) || (b + c <= a) )
-    raise TriangleError, "Sum of two sides must be higher than third one"
+  if sides_are_negative(a, b, c) 
+    raise TriangleError, "Sides must be higher than zero"
   end
 
-  if a == b && b == c
-  	return :equilateral
-
-  elsif a != b && b != c && a != c
-  	return :scalene
-
-  else
-  	return :isosceles  	
+  if triangle_not_closing(a, b, c)
+    raise TriangleError, "Sum of two sides must be higher than third one" 
   end
 end
+
+#Possible failures
+def sides_are_negative(a, b, c)
+  a <= 0 || b <= 0 || c <= 0
+end
+
+def triangle_not_closing(a, b, c)
+  ((a + b <= c) || (a + c <= b) || (b + c <= a))
+end
+#Defining side lengths
+def all_sides_are_equal(a, b, c) 
+  a == b && b == c
+end
+
+def each_side_is_different(a, b, c) 
+  a != b && b != c && a != c
+end
+
+def two_sides_are_equal(a, b, c)
+  (a==b && a!=c) || (a==c && a!=b) || (b==c && b!=a)
+end
+
 
 # Error class used in part 2.  No need to change this code.
 class TriangleError < StandardError
