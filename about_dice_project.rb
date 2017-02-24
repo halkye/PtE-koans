3706bea0885d34287bb3a9ab03ac24ddb6c75bdd
Piotr Halkiewicz <halkye@gmail.com>
2017-02-24 16:02:34 +0100

    Task was to write class DiceSet that is responsible with rolling k6 dice n times. Only method was quite easy but I was able to make it work when used instance variable @values instead of just values

--- about_dice_project.rb
+++ about_dice_project.rb
@@ -2,9 +2,15 @@ require File.expand_path(File.dirname(__FILE__) + '/neo')
 
 # Implement a DiceSet Class here:
 #
-# class DiceSet
-#   code ...
-# end
+class DiceSet
+  def values
+    @values
+  end
+
+  def roll(n)
+    @values = [1, 2, 3, 4, 5, 6].sample(n)
+  end
+end
 
 class AboutDiceProject < Neo::Koan
   def test_can_create_a_dice_set
